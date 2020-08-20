8f806b4f4751140a1ea286db8395e3d5f94287b0
ridho-darmawan <ridho.rd849@gmail.com>
2020-08-20 10:19:32 +0700

    update file sql

--- fl_arsippelanggan.sql
+++ fl_arsippelanggan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2020 at 10:34 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Generation Time: Aug 20, 2020 at 05:15 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -67,20 +67,15 @@ CREATE TABLE `tb_pelanggan` (
 --
 
 INSERT INTO `tb_pelanggan` (`Id`, `Id_pelanggan`, `nomor_agenda`, `nama`, `alamat`, `tarif`, `daya`, `tanggal_permohonan`, `status_kelengkapan_berkas`, `nomor_lemari`, `nomor_rak`, `berkas`) VALUES
-(47, '311400238455', '311409912007070266', 'CORNELIAiiqq', 'RURUKAN', 'R1T', '450 VA', '0000-00-00', 'Lengkap', 1, '2Ba', 'x.zip'),
-(48, '311400238414', '31140051200708273', 'FRETS TAMBAHANI', 'WATULAMBOT', 'R1T', '900 VA', '0000-00-00', 'Belum Lengkap', 1, '2B', 'berkas1.rar'),
-(49, '311400238584', '311400512007130301', 'FERROX ASIEL TULONG', 'KOYA', 'R1MT', '900 VA', '0000-00-00', 'Belum Lengkap', 1, '2B', 'buku1.zip'),
-(50, '311400238422', '311400512007130297', 'LUSYE GREESE MANGUND', 'KINIAR', 'R1MT', '900 VA', '0000-00-00', 'Belum Lengkap', 1, '2B', 'upload1.zip'),
-(51, '311400238471', '311400512007100287', 'MARCHEL M TUILAN', 'ERIS', 'R1T', '900 VA', '0000-00-00', 'Belum Lengkap', 1, '2B', 'upload2.zip'),
-(52, '311400238145', '311400512007060258', 'JEFRY HERMANUS', 'KAPATARAN SATU', 'R1T', '900 VA', '0000-00-00', 'Belum Lengkap', 1, '2B', 'upload3.zip'),
-(53, '311400020117', '311400522007308787', 'USMAR AHUDULU', 'RINEGETAN', 'R1', '2200 VA', '0000-00-00', 'Belum Lengkap', 1, '2B', 'upload4.zip'),
-(54, '311400238497', '311400512006250211', 'JULIUS Y SAMBUAGA', 'PARENTEK', 'R1T', '1300 VA', '0000-00-00', 'Belum Lengkap', 1, '2B', 'x.zip'),
-(62, '3140005120387', '311400512007290387', 'FELIX KUSOY', 'KINALEOSAN', 'R1MT', '900 VA', '0000-00-00', 'Belum Lengkap', 1, '2B', 'zz.zip'),
-(63, '', '', '', '', '', '450 VA', '0000-00-00', 'Lengkap', 0, '', 'zz.zip'),
-(69, '', '', '', '', '', '450 VA', '0000-00-00', 'Lengkap', 0, '', 'zz.zip'),
-(70, '', '', '', '', '', '450 VA', '0000-00-00', 'Lengkap', 0, '', 'zz.zip'),
-(71, '', '', '', '', '', '450 VA', '0000-00-00', 'Lengkap', 0, '', 'zz.zip'),
-(72, '', '', '', '', '', '450 VA', '0000-00-00', 'Lengkap', 0, '', 'zz.zip');
+(47, '311400238455', '311409912007070266', 'CORNELIAiiqq', 'RURUKAN', 'R1T', '450 VA', '2020-08-19', 'Lengkap', 1, '2Ba', 'x.zip'),
+(48, '311400238414', '31140051200708273', 'FRETS TAMBAHANI', 'WATULAMBOT', 'R1T', '450 VA', '2020-08-11', 'Lengkap', 1, '2B', 'berkas1.rar'),
+(49, '311400238584', '311400512007130301', 'FERROX ASIEL TULONG', 'KOYA', 'R1MT', '450 VA', '2020-08-19', 'Lengkap', 1, '2B', 'buku1.zip'),
+(50, '311400238422', '311400512007130297', 'LUSYE GREESE MANGUND', 'KINIAR', 'R1MT', '450 VA', '2018-07-26', 'Lengkap', 1, '2B', 'upload1.zip'),
+(51, '311400238471', '311400512007100287', 'MARCHEL M TUILAN', 'ERIS', 'R1T', '450 VA', '2020-07-29', 'Lengkap', 1, '2B', 'upload2.zip'),
+(52, '311400238145', '311400512007060258', 'JEFRY HERMANUS', 'KAPATARAN SATU', 'R1T', '450 VA', '2020-08-03', 'Lengkap', 1, '2B', 'upload3.zip'),
+(53, '311400020117', '311400522007308787', 'USMAR AHUDULU', 'RINEGETAN', 'R1', '450 VA', '2020-08-04', 'Lengkap', 1, '2B', 'upload4.zip'),
+(54, '311400238497', '311400512006250211', 'JULIUS Y SAMBUAGA', 'PARENTEK', 'R1T', '450 VA', '2020-07-28', 'Lengkap', 1, '2B', 'x.zip'),
+(62, '3140005120387', '311400512007290387', 'FELIX KUSOY', 'KINALEOSAN', 'R1MT', '450 VA', '2020-08-13', 'Lengkap', 1, '2B', 'zz.zip');
 
 --
 -- Indexes for dumped tables
@@ -112,7 +107,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `tb_pelanggan`
 --
 ALTER TABLE `tb_pelanggan`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
